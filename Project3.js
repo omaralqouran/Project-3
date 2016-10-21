81d5806ef6db82e9bb8bb8884dfa0b30c5291efa
omaralqouran <omaralqouran48@gmail.com>
2016-10-21 11:48:31 -0400

    Add files via upload

--- Project3.js
+++ Project3.js
@@ -9,41 +9,36 @@ const PROMPT = require('readline-sync');
 
 let continueResponse, continueRating;
 let movieTitle;
-let movieRating, averageRating, numberResponses, totalRating, numberRating;
+let movieRating, averageRating, totalRating, numberRating;
 
 
 function main() {
     process.stdout.write('\x1Bc'); //Clears the screen
     setContinueResponse();
 
-    while (continueResponse === 1) {
-        const MAX_RETRY_VALUE = 4;
+    while (continueResponse == 1) {
         setMovieTitle();
+        calculateTotalRating();
         setMovieRating();
-        setNumberResponses();
+        setNumberRatings();
+        calculateTotalRating();
         setAverageRating();
         setContinueResponse();
-
-        for (let i = 0; i < MAX_RETRY_VALUE; i++) {
-            doPrintGoodbye();
-        }
+        printGoodbye();
     }
 }
 
 main();
 
 function setContinueResponse() {
-    function setContinueResponse() {
         if (continueResponse != null) {
             continueResponse = 2;
             while (continueResponse !== 0 && continueResponse !== 1) {
-                continueResponse = Number(PROMPT.question(`\nDo you want to continue? [0=no, 1=yes]: `));
+                continueResponse = Number(PROMPT.question(`\nDo you want to rate a movie? [0=no, 1=yes]: `));
             }
         } else {
             continueResponse = 1;
         }
-    }
-
 }
 
 function setMovieTitle() {
@@ -51,22 +46,43 @@ function setMovieTitle() {
 }
 
 function setMovieRating() {
-    movieRating = Number(PROMPT.question(`\nOn a scale of 1 to 5 stars, with 5 being the best, what do you rate ${movieTitle}? `));
+    let answeredQuestion = 0;
+    let counter = 0;
+    const MAX_WRONG_TRIES = 3,
+        MAX_RATING = 5,
+        MIN_RATING = 1;
+    while (counter < MAX_WRONG_TRIES && answeredQuestion != "yes") {
+        movieRating = Number(PROMPT.question(`\nOn a scale of 1 to 5 stars, with 5 being the best, what do you rate it? `));
+        if (movieRating < MIN_RATING || movieRating > MAX_RATING) {
+            console.log(`\n${movieRating} is a incorrect value please try again.`);
+            counter++;
+        }
 
-}
-function setTotalRating () {
-    
-}
-
-function setNumberResponses() {
-    numberResponses ++;
+        else {
+            answeredQuestion = "yes"
+        }
+    }
 }
 
-function setAverageRating() {
-    averageRating = totalRating/numberRating
-}
+    function setNumberRatings() {
+        if (numberRating >= 1) {
+            numberRating++;
+        } else if (numberRating == null) {
+            numberRating = 1;
+        }
+    }
 
-function doPrintGoodbye() {
-    console.log(`\n\tGoodbye, we thank you for your time.`);
-}
+    function calculateTotalRating() {
+        if (totalRating = 0) {
+            totalRating = totalRating + movieRating;
+        }
+    }
 
+
+    function setAverageRating() {
+        averageRating = totalRating / numberRating;
+    }
+
+    function printGoodbye() {
+        console.log(`\n\tGoodbye, we thank you for your time.`);
+    }
