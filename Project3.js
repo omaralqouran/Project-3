cae7d504b0718bcfcec61de92b8023e5ae27107e
omaralqouran <omaralqouran48@gmail.com>
2016-10-23 22:28:49 -0400

    Add files via upload

--- Project3.js
+++ Project3.js
@@ -4,6 +4,7 @@
  *   @summary Project 3  || created: 09.27.2016
  *   @todo
  */
+
 "use strict";
 const PROMPT = require('readline-sync');
 
@@ -11,38 +12,38 @@ let continueResponse, continueRating;
 let movieTitle;
 let movieRating, averageRating, totalRating, numberRating;
 
-
 function main() {
     process.stdout.write('\x1Bc'); //Clears the screen
     setContinueResponse();
 
-    while (continueResponse == 1) {
-        setMovieTitle();
-        calculateTotalRating();
-        setMovieRating();
-        setNumberRatings();
-        calculateTotalRating();
-        setAverageRating();
-        setContinueResponse();
-        printGoodbye();
+    while (continueResponse === 1){
+        if(continueResponse = 1) {
+            setMovieTitle();
+            setMovieRating();
+            setNumberRatings();
+            calculateTotalRating();
+            setAverageRating();
+            setContinueResponse();
+            printGoodbye();
+        }
     }
 }
 
 main();
 
 function setContinueResponse() {
-        if (continueResponse != null) {
-            continueResponse = 2;
-            while (continueResponse !== 0 && continueResponse !== 1) {
-                continueResponse = Number(PROMPT.question(`\nDo you want to rate a movie? [0=no, 1=yes]: `));
-            }
-        } else {
-            continueResponse = 1;
+    if (continueResponse != null) {
+        continueResponse = -1;
+        while (continueResponse !== 0 && continueResponse !== 1) {
+            continueResponse = Number(PROMPT.question(`\nDo you want to rate a movie? [0=no, 1=yes]: `));
         }
+    } else {
+        continueResponse = 1;
+    }
 }
 
 function setMovieTitle() {
-    movieTitle = Number(PROMPT.question('\nPLease enter a movie Title: '));
+    movieTitle = PROMPT.question('\nPLease enter a movie Title: ');
 }
 
 function setMovieRating() {
@@ -51,38 +52,36 @@ function setMovieRating() {
     const MAX_WRONG_TRIES = 3,
         MAX_RATING = 5,
         MIN_RATING = 1;
-    while (counter < MAX_WRONG_TRIES && answeredQuestion != "yes") {
+    while (counter < MAX_WRONG_TRIES && answeredQuestion != 1) {
         movieRating = Number(PROMPT.question(`\nOn a scale of 1 to 5 stars, with 5 being the best, what do you rate it? `));
         if (movieRating < MIN_RATING || movieRating > MAX_RATING) {
-            console.log(`\n${movieRating} is a incorrect value please try again.`);
+            console.log(`\nDo you know how to follow directions? ${movieRating} is a incorrect value please try again.`);
             counter++;
         }
 
         else {
-            answeredQuestion = "yes"
+            answeredQuestion = 1
         }
     }
 }
 
-    function setNumberRatings() {
-        if (numberRating >= 1) {
-            numberRating++;
-        } else if (numberRating == null) {
-            numberRating = 1;
-        }
+function setNumberRatings() {
+    if (numberRating >= 1) {
+        numberRating++;
+    } else if (numberRating == null) {
+        numberRating = 1;
     }
+}
 
-    function calculateTotalRating() {
-        if (totalRating = 0) {
-            totalRating = totalRating + movieRating;
-        }
-    }
+function calculateTotalRating() {
+    const TOTAL_RATING = 0;
+        totalRating = TOTAL_RATING + movieRating;
+}
 
+function setAverageRating() {
+    averageRating = totalRating / numberRating;
+}
 
-    function setAverageRating() {
-        averageRating = totalRating / numberRating;
-    }
-
-    function printGoodbye() {
-        console.log(`\n\tGoodbye, we thank you for your time.`);
-    }
+function printGoodbye() {
+    console.log(`\n\t The average rating of ${movieTitle} is ${averageRating}. Goodbye, we thank you for your time.`);
+}
